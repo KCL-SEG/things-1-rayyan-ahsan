99cb9fe8c83e93b0d303b37866feacae30dc6796
rayyan ahsan <raygraphicsreal@gmail.com>
2022-10-12 03:45:38 +0100

    description updated

--- things/models.py
+++ things/models.py
@@ -5,5 +5,5 @@ from django.core.validators import MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(blank=False,max_length=30,unique=False)
-    description = models.CharField(max_length=120,unique=False)
+    description = models.CharField(max_length=120,unique=False,blank=True)
     quantity = models.PositiveIntegerField(MaxValueValidator(100), unique=True)
