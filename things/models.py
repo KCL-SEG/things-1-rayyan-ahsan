e3b57e3888321c165a87f210667bb0dec79eb8a7
rayyan ahsan <raygraphicsreal@gmail.com>
2022-10-12 03:02:55 +0100

    thing model added

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
+from pickle import TRUE
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 # Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    quantity = models.PositiveIntegerField(MaxValueValidator(100), unique=TRUE)
