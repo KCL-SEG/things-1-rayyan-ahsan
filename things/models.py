ff1d3bf64b2f19882a9d6f4825dad6d2a774c789
rayyan ahsan <raygraphicsreal@gmail.com>
2022-10-12 03:54:17 +0100

    quantity updated + migrations

--- things/models.py
+++ things/models.py
@@ -1,9 +1,10 @@
 from pickle import TRUE
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
     name = models.CharField(blank=False,max_length=30,unique=True)
     description = models.CharField(max_length=120,unique=False,blank=True)
-    quantity = models.PositiveIntegerField(validators=([MaxValueValidator(100)]), unique=False)
+    quantity = models.PositiveIntegerField(validators=([MinValueValidator(0),MaxValueValidator(100)]), unique=False)
