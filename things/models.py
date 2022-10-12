af3fb7e619653e9ec36fc8b8a408d473e239bbf6
rayyan ahsan <raygraphicsreal@gmail.com>
2022-10-12 03:46:52 +0100

    Unique updated on name

--- things/models.py
+++ things/models.py
@@ -4,6 +4,6 @@ from django.core.validators import MaxValueValidator
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(blank=False,max_length=30,unique=False)
+    name = models.CharField(blank=False,max_length=30,unique=True)
     description = models.CharField(max_length=120,unique=False,blank=True)
     quantity = models.PositiveIntegerField(MaxValueValidator(100), unique=True)
